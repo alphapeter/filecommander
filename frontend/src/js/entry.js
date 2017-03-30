3ea7b344be6178303cac19c5f8a19453d61477d7
Peter <peter@otocolobus.se>
2017-03-30 21:33:05 +0200

    vue

--- frontend/src/js/entry.js
+++ frontend/src/js/entry.js
@@ -1,3 +1,20 @@
+var Vue = require("./vue2.2.5.js");
 require("../css/main.css");
 require("../css/font-icons.css");
+
+
+new Vue({
+    el: `#app`,
+    data: {
+        title: `hello world!`
+    },
+    methods: {
+        changeTitle: function(event) {
+            this.title = event.target.value;
+        }
+    }
+});
+
+
+
 
